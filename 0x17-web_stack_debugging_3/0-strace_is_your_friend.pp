3b29165ab66eb5477f8d9cd9bf2282e3ba6b98ac
Arnthorny <chidubem.anthony26@gmail.com>
2023-11-08 22:01:42 -0800

    Used exc resource in place of file_line resource

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,15 +1,9 @@
 #Fix Apache  returning a 500 error
-include stdlib
 
 $config_file = '/var/www/html/wp-settings.php'
-$replacer = "require_once( ABSPATH . WPINC . '/class-wp-locale.php' );"
 
 
 #This resource makes required replacement
-file_line { 'Load library':
-  ensure   => present,
-  path     => $config_file,
-  line     => $replacer,
-  match    => '.*class-wp-locale.phpp.*',
-  multiple => false,
+exec { 'fix-wordpress':
+  command => "/bin/sed -E -i 's/.phpp/.php/' ${config_file}"
 }
