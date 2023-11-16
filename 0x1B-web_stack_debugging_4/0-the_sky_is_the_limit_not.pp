ce0acea9f8513d5f82c5f6856e3a7887aa81875a
Arnthorny <chidubem.anthony26@gmail.com>
2023-11-16 05:22:21 -0800

    Added resource to restart nginx

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -5,5 +5,11 @@ $config_file = '/etc/default/nginx'
 
 #This resource removes last line from file
 exec { 'fix--for-nginx':
-  command => "/bin/sed -i '\$d' ${config_file}"
+  command => "/bin/sed -i '\$d' ${config_file}",
+  before  => Exec['restart-nginx']
+}
+
+# This service resource declares the nginx service
+exec { 'restart-nginx':
+  command => '/usr/sbin/service nginx restart'
 }
