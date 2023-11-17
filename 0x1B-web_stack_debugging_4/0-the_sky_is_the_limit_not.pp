4af8b9f195aac84f8fa0eb21a0906b9fabf0cddd
Arnthorny <chidubem.anthony26@gmail.com>
2023-11-16 17:55:33 -0800

    Added new ulimit with puppet resource

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -5,11 +5,13 @@ $config_file = '/etc/default/nginx'
 
 #This resource removes last line from file
 exec { 'fix--for-nginx':
-  command => "/bin/sed -i '\$d' ${config_file}",
-  before  => Exec['restart-nginx']
+  provider => shell,
+  command  => "sed -i '\$d' ${config_file} && echo 'ULIMIT=\"-n 4096\"' >> ${config_file}",
+  before   => Exec['restart-nginx']
 }
 
 # This service resource declares the nginx service
 exec { 'restart-nginx':
-  command => '/usr/sbin/service nginx restart'
+  provider => shell,
+  command  => 'service nginx restart'
 }
